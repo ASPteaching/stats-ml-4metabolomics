b9d4fa06d53f8e7fe77ad6a6dccd90a487e686d9
Alejandro Sanchez Pla <asanchez@ub.edu>
2024-06-14 13:59:30 +0200

    Updated ANN presentation

---
+++ labs/MetaboAnalystR/metaboAnalystInstall.R
@@ -0,0 +1,30 @@
+# Installing dependencies
+
+metanr_packages <- function(){
+  metr_pkgs <- c("impute", "pcaMethods", "globaltest", "GlobalAncova", "Rgraphviz", "preprocessCore", "genefilter", "SSPA", "sva", "limma", "KEGGgraph", "siggenes","BiocParallel", "MSnbase", "multtest", "RBGL", "edgeR", "fgsea", "devtools", "crmn")
+  list_installed <- installed.packages()
+  new_pkgs <- subset(metr_pkgs, !(metr_pkgs %in% list_installed[, "Package"]))
+  if(length(new_pkgs)!=0){if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+    BiocManager::install(new_pkgs)
+    print(c(new_pkgs, " packages added..."))
+  }
+  
+  if((length(new_pkgs)<1)){
+    print("No new packages added...")
+  }
+}
+
+metanr_packages()
+
+# 2 Install the package
+
+# Step 1: Install devtools
+install.packages("devtools")
+library(devtools)
+
+# Step 2: Install MetaboAnalystR without documentation
+devtools::install_github("xia-lab/MetaboAnalystR", build = TRUE, build_vignettes = FALSE)
+
+# Step 2: Install MetaboAnalystR with documentation
+devtools::install_github("xia-lab/MetaboAnalystR", build = TRUE, build_vignettes = TRUE, build_manual =T)
